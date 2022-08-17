9bd621b43528839e3d41123dfdcdff07d06dd6c2
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-08-17 22:06:59 +0000

    Add config

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import vercel from '@sveltejs/adapter-vercel';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config}} */
+const config = {
+	preprocess: preprocess(),
+	kit: {
+		adapter: vercel({
+			edge: true,
+			external: [],
+			split: true
+		})
+	}
+};
+
+export default config;
