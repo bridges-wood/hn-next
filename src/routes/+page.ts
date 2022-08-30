00fee6607cf11d60b0d0827d7cb4ceb0a11c413a
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-08-30 19:07:45 +0000

    Refactor api to use provided fetch

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,16 +1,16 @@
 import type { Item } from '$typings/index';
-import { getItem, getStories } from '$utils/api';
+import { getItem, getStoryIDs } from '$utils/api';
 import type { PageLoad } from './$types';
 
 const NUM_ITEMS = 30;
 
-export const load: PageLoad = async ({ request }) => {
+export const load: PageLoad = async ({ fetch }) => {
 	// Fetch top story ids
-	const storyIDs = await getStories('top', 3);
+	const storyIDs = await getStoryIDs('top', NUM_ITEMS, 0, fetch);
 	console.log('items', storyIDs);
 	// Fetch top stories
 	const stories: Item[] = await Promise.all(
-		storyIDs.slice(0, NUM_ITEMS).map(async (id: number) => await getItem(id))
+		storyIDs.slice(0, NUM_ITEMS).map(async (id: number) => await getItem(id, fetch))
 	);
 
 	return { stories };
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,18 +4,26 @@ const HN_DATABASE_URL = 'https://hacker-news.firebaseio.com';
 const HN_VERSION = 'v0';
 const HN_URL = `${HN_DATABASE_URL}/${HN_VERSION}`;
 
-export const getItem = async (id: Item['id']): Promise<Item> => {
-	const res = await fetch(`${HN_URL}/item/${id}.json`);
+export const getItem = async (
+	id: Item['id'],
+	fetchFunc: (info: RequestInfo, init?: RequestInit | undefined) => Promise<Response> = fetch
+): Promise<Item> => {
+	const res = await fetchFunc(`${HN_URL}/item/${id}.json`);
 	return await res.json();
 };
 
-export const getUser = async (id: User['id']): Promise<User> => {
-	const res = await fetch(`${HN_URL}/user/${id}.json`);
+export const getUser = async (
+	id: User['id'],
+	fetchFunc: (info: RequestInfo, init?: RequestInit | undefined) => Promise<Response> = fetch
+): Promise<User> => {
+	const res = await fetchFunc(`${HN_URL}/user/${id}.json`);
 	return await res.json();
 };
 
-export const getMaxItemID = async (): Promise<Item['id']> => {
-	const res = await fetch(`${HN_URL}/maxitem.json`);
+export const getMaxItemID = async (
+	fetchFunc: (info: RequestInfo, init?: RequestInit | undefined) => Promise<Response> = fetch
+): Promise<Item['id']> => {
+	const res = await fetchFunc(`${HN_URL}/maxitem.json`);
 	return await res.json();
 };
 
@@ -27,20 +35,40 @@ export const getMaxItemID = async (): Promise<Item['id']> => {
  * @param offset The number of stories to skip.
  * @returns A promise that resolves to an array of stories.
  */
-export const getStories = async (
+export const getStoryIDs = async (
 	type: 'top' | 'best' | 'new' | 'ask' | 'show' | 'job' = 'top',
 	limit = 10,
-	offset = 0
+	offset = 0,
+	fetchFunc: (info: RequestInfo, init?: RequestInit | undefined) => Promise<Response> = fetch
 ): Promise<Item['id'][]> => {
-	const res = await fetch(`${HN_URL}/${type}stories.json`);
+	const res = await fetchFunc(`${HN_URL}/${type}stories.json`);
 	const stories = await res.json();
 	return stories.slice(offset, offset + limit);
 };
 
-export const getUpdates = async (): Promise<{
+/**
+ * Helper function to get an array of populated stories.
+ * @param type The type of stories to get.
+ * @param limit The number of stories to get.
+ * @param offset The number of stories to skip.
+ * @returns A promise that resolves to an array of stories.
+ */
+export const getPopulatedStories = async (
+	type: 'top' | 'best' | 'new' | 'ask' | 'show' | 'job' = 'top',
+	limit = 10,
+	offset = 0,
+	fetchFunc: (info: RequestInfo, init?: RequestInit | undefined) => Promise<Response> = fetch
+): Promise<Item[]> => {
+	const storyIDs = await getStoryIDs(type, limit, offset, fetchFunc);
+	return Promise.all(storyIDs.map(async (id: Item['id']) => await getItem(id)));
+};
+
+export const getUpdates = async (
+	fetchFunc: (info: RequestInfo, init?: RequestInit | undefined) => Promise<Response> = fetch
+): Promise<{
 	items: Item['id'][];
 	profiles: User['id'][];
 }> => {
-	const res = await fetch(`${HN_URL}/updates.json`);
+	const res = await fetchFunc(`${HN_URL}/updates.json`);
 	return await res.json();
 };
