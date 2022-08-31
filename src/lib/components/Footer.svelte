c0fc0ef4d652a7a5a4a4e209ddfa39990de007ee
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-08-31 15:40:45 +0000

    Update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,15 @@
+<script>
+	import AnimatedLink from '$lib/components/AnimatedLink.svelte';
+</script>
+
 <footer>
-	Created by <a href="https://maxwood.tech" target="__blank">Max Wood</a>
+	Created by <AnimatedLink href="https://maxwood.tech" target="__blank">Max Wood</AnimatedLink>
 </footer>
+
+<style>
+	footer {
+		text-align: center;
+		font-size: 0.75rem;
+		margin-top: 20px;
+	}
+</style>
