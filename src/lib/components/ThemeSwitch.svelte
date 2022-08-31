1ffe099e047d765f29033ce761c902fe39a294f4
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-08-31 21:27:41 +0000

    Fix ThemeSwitch to use store value

--- src/lib/components/ThemeSwitch.svelte
+++ src/lib/components/ThemeSwitch.svelte
@@ -1,8 +1,102 @@
 <script lang="ts">
-	import themeStore from 'svelte-themes';
+	import themeStore, { setTheme } from 'svelte-themes';
 </script>
 
-<select bind:value={$themeStore.theme}>
-	<option value="light">Light</option>
-	<option value="dark">Dark</option>
-</select>
+<div class={$$props.class}>
+	<input
+		type="checkbox"
+		checked={$themeStore.theme === 'dark'}
+		on:change={(e) => {
+			setTheme(e?.currentTarget?.checked ? 'dark' : 'light');
+		}}
+		id="theme-switch"
+	/>
+	<label for="theme-switch">
+		<svg class="sun" viewBox="0 0 24 24"
+			><path
+				d="M6.993 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007S14.761 6.993 12 6.993 6.993 9.239 6.993 12zM12 8.993c1.658 0 3.007 1.349 3.007 3.007S13.658 15.007 12 15.007 8.993 13.658 8.993 12 10.342 8.993 12 8.993zM10.998 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2h-3zm17 0h3v2h-3zM4.219 18.363l2.12-2.122 1.415 1.414-2.12 2.122zM16.24 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.342 7.759 4.22 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z"
+			/></svg
+		>
+		<svg class="moon" viewBox="0 0 24 24"
+			><path
+				d="M20.742 13.045a8.088 8.088 0 0 1-2.077.271c-2.135 0-4.14-.83-5.646-2.336a8.025 8.025 0 0 1-2.064-7.723A1 1 0 0 0 9.73 2.034a10.014 10.014 0 0 0-4.489 2.582c-3.898 3.898-3.898 10.243 0 14.143a9.937 9.937 0 0 0 7.072 2.93 9.93 9.93 0 0 0 7.07-2.929 10.007 10.007 0 0 0 2.583-4.491 1.001 1.001 0 0 0-1.224-1.224zm-2.772 4.301a7.947 7.947 0 0 1-5.656 2.343 7.953 7.953 0 0 1-5.658-2.344c-3.118-3.119-3.118-8.195 0-11.314a7.923 7.923 0 0 1 2.06-1.483 10.027 10.027 0 0 0 2.89 7.848 9.972 9.972 0 0 0 7.848 2.891 8.036 8.036 0 0 1-1.484 2.059z"
+			/></svg
+		>
+	</label>
+</div>
+
+<style>
+	label {
+		width: 55px;
+		height: 25px;
+		position: relative;
+		display: block;
+		background: var(--neutralSubtleContrast);
+		border-radius: 55px;
+		cursor: pointer;
+	}
+
+	label svg {
+		display: block;
+		position: absolute;
+		width: 20px;
+		height: 20px;
+		top: 2.5px;
+		z-index: 100;
+	}
+
+	label svg.sun {
+		left: 2.5px;
+		fill: var(--white);
+		transition: 0.3s;
+	}
+
+	label svg.moon {
+		left: 32.5px;
+		fill: var(--gray8);
+		transition: 0.3s;
+	}
+
+	input:checked + label svg.sun {
+		fill: var(--gray8);
+	}
+
+	input:checked + label svg.moon {
+		fill: var(--white);
+	}
+
+	label::after {
+		content: '';
+		width: 25px;
+		height: 25px;
+		left: 0px;
+		top: 0px;
+		position: absolute;
+
+		background: var(--primarySolid);
+		border-radius: 50px;
+	}
+
+	input {
+		display: none;
+	}
+
+	input:checked + label {
+		background: #242424;
+	}
+
+	input:checked + label::after {
+		left: 54px;
+		transform: translateX(-100%);
+		background: red;
+	}
+
+	label,
+	label::after {
+		transition: 0.3s;
+	}
+
+	label:active::after {
+		width: 31px;
+	}
+</style>
