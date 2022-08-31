35d05f2b6a3b182369d298451e5a589ae483c9e9
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-08-31 15:40:51 +0000

    Create header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import ThemeSwitch from '$lib/components/ThemeSwitch.svelte';
+</script>
+
+<header>
+	<ThemeSwitch />
+</header>
+
+<style>
+	header {
+		display: flex;
+		flex-direction: row;
+	}
+</style>
