6d6de76d948463f017cac207eede52c239c50d8d
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-08-31 21:27:55 +0000

    Update Header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,12 +3,22 @@
 </script>
 
 <header>
-	<ThemeSwitch />
+	<ThemeSwitch class="theme-switch" />
 </header>
 
 <style>
 	header {
+		box-sizing: border-box;
 		display: flex;
 		flex-direction: row;
+		width: 100%;
+		padding: 20px;
+	}
+
+	header :global(.theme-switch) {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		margin-left: auto;
 	}
 </style>
