bea052ec083ab0391b7ea5be7d4c3299835ea1a6
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-08-31 15:41:08 +0000

    Create Comment, Poll and PollOpt components

---
+++ src/lib/components/items/Comment.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Item } from '$typings/index';
+
+	export let data: Item;
+	if (data.type !== 'comment') throw new Error('Not a comment');
+	console.log('data', data);
+</script>
+
+<div>
+	{@html data.text}
+</div>
---
+++ src/lib/components/items/Poll.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import type { Item } from '$typings/index';
+
+	export let data: Item;
+</script>
