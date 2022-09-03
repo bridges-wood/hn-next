1829cc280bd2b8d57816014490b60ed8cb78a599
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-09-03 21:26:41 +0000

    Refactor comments page

--- src/routes/items/[slug]/+page.svelte
+++ src/routes/items/[slug]/+page.svelte
@@ -1,19 +1,58 @@
 <script lang="ts">
+	import AnimatedLink from '$lib/components/AnimatedLink.svelte';
 	import Comment from '$lib/components/items/Comment.svelte';
+	import { calculateAndFormatTimeDifference } from '$utils/time';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	console.log('data', data.kids.length);
+	const timeDifference = calculateAndFormatTimeDifference(data.item.time);
+	const { title, url, score, by: author, descendants, id, type } = data.item;
 </script>
 
 <svelte:head>
-	<title>{data?.item.title}</title>
+	<title>HN Next &VerticalLine; {data?.item.title}</title>
 </svelte:head>
-<h1>{data?.item.title}</h1>
-<div>
-	{#each data?.kids as child}
-		{#if child.type === 'comment'}
-			<Comment data={child} />
-		{/if}
-	{/each}
-</div>
+<AnimatedLink href={data.item.url}><h1>{data?.item.title}</h1></AnimatedLink>
+<small class="item__subtext">
+	{#if score}
+		{score} {score === 1 ? 'point' : 'points'}
+	{/if}
+	{#if author}
+		by {author}
+	{/if}
+	{#if timeDifference}
+		{timeDifference}
+	{/if}
+	{#if descendants}
+		| <AnimatedLink href={`/items/${id}`}>
+			{descendants}
+			{descendants === 1 ? 'comment' : 'comments'}
+		</AnimatedLink>
+	{/if}
+</small>
+{#if data?.item.text}
+	{@html data.item.text}
+{/if}
+{#if data?.kids}
+	<div class="comments">
+		{#each data?.kids.sort((a, b) => Number(b.time) - Number(a.time)) as child}
+			{#if child.type === 'comment' && child.text}
+				<Comment data={child} />
+			{/if}
+		{/each}
+	</div>
+{/if}
+
+<style>
+	.comments {
+		display: flex;
+		flex-direction: column;
+		gap: 12px;
+	}
+
+	.item__subtext {
+		font-size: 12px;
+		color: var(--neutralLowContrastText);
+		margin-bottom: 12px;
+	}
+</style>
