6f0ec1775a5c5d2393893cc77ca0565d1b38c92c
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-09-03 21:26:01 +0000

    Update comment

--- src/lib/components/items/Comment.svelte
+++ src/lib/components/items/Comment.svelte
@@ -1,11 +1,41 @@
 <script lang="ts">
 	import type { Item } from '$typings/index';
+	import { calculateAndFormatTimeDifference } from '$utils/time';
+	import AnimatedLink from '../AnimatedLink.svelte';
 
 	export let data: Item;
 	if (data.type !== 'comment') throw new Error('Not a comment');
-	console.log('data', data);
+
+	const timeDifference = calculateAndFormatTimeDifference(data.time);
+	const { text, by: author, id } = data;
 </script>
 
-<div>
-	{@html data.text}
+<div class="comment">
+	<div class="comment-info">
+		<AnimatedLink href={`/users/${author}`}><h4>{author}</h4></AnimatedLink>
+		{#if timeDifference}
+			<small>{timeDifference}</small>
+		{/if}
+	</div>
+	{@html text}
 </div>
+
+<style>
+	.comment {
+		padding: 20px 12px;
+		border-radius: 12px;
+		background-color: var(--primarySubtleBackground);
+		width: 100%;
+	}
+
+	.comment-info {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 12px;
+	}
+
+	.comment-info h4 {
+		margin: 0;
+	}
+</style>
