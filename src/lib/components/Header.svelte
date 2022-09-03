370d5949ac9a0561a54e56e2ca9956c2a25a2e3e
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-09-03 21:23:31 +0000

    Add logo to header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,6 +3,9 @@
 </script>
 
 <header>
+	<a href="/" class="home-link">
+		<img src="/logo.svg" class="logo" alt="" />
+	</a>
 	<ThemeSwitch class="theme-switch" />
 </header>
 
@@ -11,6 +14,7 @@
 		box-sizing: border-box;
 		display: flex;
 		flex-direction: row;
+		justify-content: space-between;
 		width: 100%;
 		padding: 20px;
 	}
@@ -20,5 +24,26 @@
 		flex-direction: column;
 		justify-content: center;
 		margin-left: auto;
+	}
+
+	@media (min-width: 1280px) {
+		header {
+			max-width: 1280px;
+		}
+	}
+	@media (min-width: 1400px) {
+		header {
+			max-width: 1400px;
+		}
+	}
+
+	.home-link {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.logo {
+		height: 40px;
 	}
 </style>
