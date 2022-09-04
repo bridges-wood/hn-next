10b125aaa6f3cb2d84ee525f9c846953e8d1066e
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-09-04 16:34:33 +0000

    Add error page for users route

---
+++ src/routes/users/[slug]/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="error-message">
+	<h1>Error: {$page.status}</h1>
+	<p>Failed to load user profile</p>
+</div>
+
+<style>
+	.error-message {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
