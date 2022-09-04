de87a0cea1cd4c026603bbba7f2d90084003853e
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-09-04 16:34:46 +0000

    Change underline for animated link

--- src/lib/components/AnimatedLink.svelte
+++ src/lib/components/AnimatedLink.svelte
@@ -10,7 +10,7 @@
 <style>
 	a {
 		color: inherit;
-		text-decoration: underline #ffffff00 0.2rem;
+		text-decoration: underline #ffffff00 0.1em;
 		transition: text-decoration-color 300ms;
 	}
 	a:hover {
